4a0a31384b7aed754a7738098a51f7a34ff2e37d
Dusan Klinec <dusan.klinec@gmail.com>
2016-12-07 21:16:25 +0100

    term, poly remap; poly reduce, naive poly sim

--- polyverif/common.py
+++ polyverif/common.py
@@ -112,14 +112,18 @@ class TermEval(object):
         # evaluated base terms of deg=1
         self.base = []
 
-    def gen_term(self, indices):
+    def gen_term(self, indices, blocklen=None):
         """
         Generates a bit term mask from the indices created by term_generator().
         blocklen wide.
         :param indices: array of bit indices, e.g., [0,8] -> x0x8
+        :param blocklen: length of the term bit representation. If none, default block len is used
         :return: bit representation of the term
         """
-        term = BitArray(uint=0, length=self.blocklen)
+        if blocklen is None:
+            blocklen = self.blocklen
+
+        term = BitArray(uint=0, length=blocklen)
         for bitpos in indices:
             term[bitpos] = 1
         return term
@@ -232,6 +236,7 @@ class TermEval(object):
         """
         Probability of term t1 XOR t2 being 1 if t1 is 1 with p1 and t2 is 1 with p2.
         t1 and t2 has to be independent (no common sub-term).
+        Due to associativity can be computed on multiple terms: t1 ^ t2 ^ t3 ^ t4 = (((t1 ^ t2) ^ t3) ^ t4) - zipping.
 
         XOR:
           a b | r
@@ -247,12 +252,135 @@ class TermEval(object):
         """
         return p1*(1-p2)+(1-p1)*p2
 
-    def expp_poly(self, poly):
+    def term_remap(self, term):
         """
-        Computes expected probability of result=1 of the given polynomial under null hypothesis of uniformity.
+        Remaps the term to the lower indices - for simulation.
+        remapping may lead to degre reduction, e.g., x1x2x3x3 -> x0x1x2
+        :param term:
+        :return:
+        """
+        return range(0, len(set(term)))
+
+    def poly_remap(self, poly):
+        """
+        Remaps the polynomial to lower indices
+        e.g., x7x8x9 + x110x112 -> x0x1x2 + x3x4
         :param poly:
         :return:
         """
+        # mapping phase
+        idx = 0
+        idx_map_rev = {}  # orig idx -> new idx
+
+        res_poly = []
+        for term in poly:
+            res_term = []
+            for bitpos in term:
+                if bitpos not in idx_map_rev:
+                    idx_map_rev[bitpos] = idx
+                    res_term.append(idx)
+                    idx += 1
+                else:
+                    res_term.append(idx_map_rev[bitpos])
+
+        return res_poly, idx_map_rev
+
+    def poly_reduce(self, poly, neg, idx, val):
+        """
+        Reduces normed polynomial = fixes variable with bitpos=idx to val.
+        neg represents +1 terms
+        :param poly:
+        :param idx:
+        :return: (poly, neg)
+        """
+        res_poly = []
+        for term in poly:
+            # variable not in term - add to the polynomial unmodified
+            if idx not in term:
+                res_poly.append(term)
+                continue
+
+            # idx is in the term. remove
+            if val == 0:
+                # val is 0 -> whole term is zero, do not add.
+                continue
+
+            # val is 1 -> remove from term
+            n_term = [x for x in term if x != idx]
+
+            if len(n_term) == 0:
+                # term is empty -> is 1, neg
+                neg = not neg
+
+            else:
+                # term is non-empty, add to poly
+                res_poly.append(n_term)
+        return res_poly, neg
+
+    def expnum_poly_sim(self, poly):
+        """
+        Simulates the given polynomial w.r.t. null hypothesis, for all variable values combinations.
+        O(2^n)
+        :param poly:
+        :return:
+        """
+        npoly, idx_map_rev = self.poly_remap(poly)
+        return self.expnum_poly_sim_norm(poly, idx_map_rev)
+
+    def expnum_poly_sim_norm(self, poly, idx_map_rev):
+        """
+        Computes how many times the given polynomial evaluates to 1 for all variable combinations.
+        :param poly:
+        :param idx_set:
+        :return:
+        """
+        # current evaluation is simple - iterate over all possible values of variables.
+        deg = len(idx_map_rev)
+        is_one = 0
+
+        gen = pos_generator(dim=deg, maxelem=1)
+        for val in gen:
+            # val is [x0,x1,x2,x3,x4,...] current variable value vector.
+            # res = current polynomial evaluation
+            res = 0
+
+            # for each term &&-operation
+            for term in poly:
+                cval = 1
+                for idx in term:
+                    if val[idx] == 0:
+                        cval = 0
+                        break
+                res ^= cval
+
+            if res > 0:
+                is_one += 1
+
+        return is_one
+
+    def expp_poly(self, poly):
+        """
+        Computes expected probability of result=1 of the given polynomial under null hypothesis of uniformity.
+        Due to non-independence between terms this evaluation can take some time - simulating.
+        Independent terms are simulated, i.e. all variable combinations are computed.
+        :param poly:
+        :return:
+        """
+        # Optimization: find independent terms, move aside, can be evaluated without simulation.
+        # independent terms are XORed with expp_xor_indep()
+        #
+        # Non-independent terms need to be evaluated together. Naive method is trying all combinations of involved
+        # variables and compute number of 1s.
+        #
+        # Optimization can be done if this is performed recursively: let x1=0, (and x1=1 (two rec. branches))
+        # then reduce polynomial and apply recursively. Often a lot of branches can be pruned from the computation as
+        # it leads to 0 due to ANDs. For non-zero branches, number of 1 evaluations need to be computed.
+        # same principle can be used for this.
+        #
+        # Find independent terms.
+
+
+
 
 
 
