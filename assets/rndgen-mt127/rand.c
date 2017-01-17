03bb965030214f9e27a4a358d403de3ba7d764b3
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-17 23:51:50 +0100

    MersenneTwister TinyMT127 generator added

---
+++ assets/rndgen-mt127/rand.c
@@ -0,0 +1,38 @@
+/**
+ * Generating random numbers in C using Mersenne Twister with 2^{127} - 1 period
+ * length using TinyMT project (https://github.com/MersenneTwister-Lab/TinyMT)
+ * gcc -o rand -Wall rand.c
+ */
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include "tinymt32.h"
+
+#define BUFSIZE 1024
+
+int main(int argc, char * argv[]){
+    tinymt32_t state;
+    if (argc == 1){
+	tinymt32_init(&state, time(NULL));
+    } else {
+        tinymt32_init(&state, atoi(argv[1]));
+    }
+
+    // Generates random numbers ad infinitum.
+    char buff[BUFSIZE];
+    for(;;){
+        for(int i = 0; i<BUFSIZE/4; i+=4){
+            uint32_t cur = tinymt32_generate_uint32(&state);
+	    buff[0] = cur & 0xff;
+	    buff[1] = (cur >> 8)  & 0xff;
+	    buff[2] = (cur >> 16) & 0xff;
+	    buff[3] = (cur >> 24) & 0xff;
+        }
+        write(fileno(stdout), buff, BUFSIZE);
+	fflush(stdout);
+    }
+
+    return 0;
+}
---
+++ assets/rndgen-mt127/readme.txt
@@ -0,0 +1,5 @@
+Uses simple Mersenne Twister random number generator - tiny version.
+https://github.com/MersenneTwister-Lab/TinyMT
+
+Has only 127 bit state, 2^127 - 1 period length.
+
