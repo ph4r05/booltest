e332ada87c6acdd64d6a9a3ded9232d195cde128
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-21 21:00:44 +0100

    randc test

---
+++ randc-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python polyverif/main.py /Volumes/EXTDATA/funcdata/randc_seed1489812481 --degree 2 --block 256 --top 128 --combine-deg 2 --tv $((107286400)) --rounds 0 --poly-file polynomials-randc-seed0.txt
