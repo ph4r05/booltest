9990fc9fa8f83e3bd3f0f49d9b22f8335f950140
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-24 13:51:17 +0100

    submit scripts added

---
+++ assets/experiment-submit-100M.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FILE=$1
+FNAME="${FILE}.data"
+
+if [ ! -f $FNAME ]; then
+    echo "Error, file $FNAME" not found
+    exit 1
+fi
+
+for MB in 1 10 50 75 100
+do
+    submit-experiment -e ph4r05@gmail.com -n "${FILE}_${MB}M" -c "/home/sample-configs/${MB}MB.json" -f "${FNAME}" -a
+done
+
+
+
+
