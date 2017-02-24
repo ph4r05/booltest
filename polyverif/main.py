5ddf9cabb3adeabf9f1f54ebdaa09a106e60069b
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-24 02:34:15 +0100

    final log added - time trace

--- polyverif/main.py
+++ polyverif/main.py
@@ -614,6 +614,7 @@ class App(object):
 
             if fref is not None:
                 fref.close()
+        logger.info('Processing finished')
 
     def main(self):
         logger.debug('App started')
