5b85426e9e5a524136c7f3ded3bdb6cc837bffaf
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-27 18:27:18 +0100

    yes test scripts prepared

---
+++ assets/test-aes-size.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TOPK=128
+TESTS=1000
+
+for DATA in $((1024*1024*1)) $((1024*1024*10)) $((1024*1024*100)); do
+for block in 128 256; do
+    for deg in 1 2 3; do
+        for k in 1 2 3; do
+            echo "===================================================================================="
+            echo "Computing block: $block, deg: $deg, k: $k, date: `date`"
+            python ~/poly-verif/polyverif/randverif.py --block ${block} --deg ${deg} --rounds 0 \
+                    --tv ${DATA} --combine-deg ${k} --top ${TOPK} --test-aes  --tests ${TESTS} \
+                    --csv-zscore --no-comb-and --only-top-comb \
+                    > ~/aessize-${block}bl-${deg}deg-${k}k-${DATA}B-${TESTS}tests.out \
+                    2> ~/aessize-${block}bl-${deg}deg-${k}k-${DATA}B-${TESTS}tests.err
+
+            echo "*** Finished Computing block: $block, deg: $deg, k: $k, date: `date`"
+        done
+    done
+done
+echo "Done `date`"
+
---
+++ assets/test-aes-statistics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATA=$((1024*1024*10))
+TOPK=128
+TESTS=1000
+
+for block in 128 256 384 512; do
+    for deg in 1 2 3; do
+        for k in 1 2 3; do
+            echo "===================================================================================="
+            echo "Computing block: $block, deg: $deg, k: $k, date: `date`"
+            python ~/poly-verif/polyverif/randverif.py --block ${block} --deg ${deg} --rounds 0 \
+                    --tv ${DATA} --combine-deg ${k} --top ${TOPK} --test-aes  --tests ${TESTS} \
+                    --csv-zscore --no-comb-and --only-top-comb \
+                    > ~/aestest-${block}bl-${deg}deg-${k}k-${DATA}B-${TESTS}tests.out \
+                    2> ~/aestest-${block}bl-${deg}deg-${k}k-${DATA}B-${TESTS}tests.err
+
+            echo "*** Finished Computing block: $block, deg: $deg, k: $k, date: `date`"
+        done
+    done
+done
+echo "Done `date`"
+
