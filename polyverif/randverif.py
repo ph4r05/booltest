1a7662e1d17553e4f6d60d7a3efc1b82abfc8a6f
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-15 02:47:02 +0100

    randverify - removing redundant imports

--- polyverif/randverif.py
+++ polyverif/randverif.py
@@ -16,10 +16,6 @@ import random
 import json
 import types
 import collections
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy
 import scipy.misc
 import scipy.stats
 import subprocess
