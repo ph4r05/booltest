2d3477e379bcf66409831515b58d1f84e5f0c7bb
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-15 02:55:40 +0100

    more memory optimised benchmark script

--- assets/test-benchmark.sh
+++ assets/test-benchmark.sh
@@ -7,7 +7,7 @@ python ~/poly-verif/polyverif/testbed.py \
     --result-dir ~/testbed-results \
     --tests-manuals 2 --tests-stride 0 \
     --top ${TOPK} \
-    --no-comb-and --only-top-comb --only-top-deg --no-term-map --topterm-heap --topterm-heap-k 256 \
+    --no-comb-and --only-top-comb --only-top-deg --no-term-map --topterm-heap --topterm-heap-k 256 --best-x-combs 128 \
     > ~/benchtest.out \
     2> ~/benchtest.err
 
