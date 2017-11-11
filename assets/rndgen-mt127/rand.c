7e025cb56833aa59d01cebad02ef5571b970862d
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-11 18:36:27 +0100

    mtrand gen - comments used for debugging added

--- assets/rndgen-mt127/rand.c
+++ assets/rndgen-mt127/rand.c
@@ -14,17 +14,29 @@
 
 int main(int argc, char * argv[]){
     tinymt32_t state;
+
+//    for(int i=0; i<4;i++){
+//    printf("  %x\n", state.status[i]);
+//    }
+
     if (argc == 1){
 	    tinymt32_init(&state, 0);
     } else {
         tinymt32_init(&state, (uint32_t)atoi(argv[1]));
     }
 
+//    printf("%x\n", TINYMT32_MASK);
+//    for(int i=0; i<4;i++){
+//    printf("  %x\n", state.status[i]);
+//    }
+
     // Generates random numbers ad infinitum.
     char buff[BUFSIZE+64];
     for(;;){
         for(int i = 0; i<BUFSIZE; i+=4){
             uint32_t cur = tinymt32_generate_uint32(&state);
+//            printf("%x\n", cur);
+//            return 1;
             buff[i+0] = cur & 0xff;
             buff[i+1] = (cur >> 8)  & 0xff;
             buff[i+2] = (cur >> 16) & 0xff;
