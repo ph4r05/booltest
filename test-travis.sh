8f64a752a5aeff0c5dd4a027b3ec9f38f6c10a22
Dusan Klinec <dusan.klinec@gmail.com>
2017-11-11 21:02:27 +0100

    travis - simple dd

--- test-travis.sh
+++ test-travis.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 python -m unittest discover $*
+
+# Simple test - random file vs zero file.
+#   Correctness not yet tested.
+
+dd if=/dev/urandom of=random-file.bin bs=1024 count=$((1024*10))
+dd if=/dev/zero of=zero-file.bin bs=1024 count=$((1024*10))
+
+booltest --degree 2 --block 256 --top 128 --tv $((1024*1024*10)) --rounds 0 random-file.bin
+booltest --degree 2 --block 256 --top 128 --tv $((1024*1024*10)) --rounds 0 zero-file.bin
