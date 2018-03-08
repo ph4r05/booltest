b851acdfe76696e933e0de5d51d7e8c742453955
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-09 00:08:40 +0100

    booltest metacentrum uses py3.6

--- assets/booltest-metacentrum.sh
+++ assets/booltest-metacentrum.sh
@@ -2,6 +2,7 @@
 
 export HOMEDIR="/storage/brno3-cerit/home/${LOGNAME}"
 export BOOLTEST="${HOMEDIR}/booltest"
+export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams"
 cd $HOMEDIR
 
 export MPICH_NEMESIS_NETMOD=tcp
@@ -10,10 +11,16 @@ export PYENV_ROOT="${HOMEDIR}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 echo "`hostname` starting..."
 
+module add gcc-5.3.0
+module add cmake-3.6.1
+module add mpc-1.0.3
+module add gmp-6.1.2
+module add mpfr-3.1.4
+
 eval "$(pyenv init -)"
 sleep 3
 
-pyenv local 2.7.14
+pyenv local 3.6.4
 sleep 3
 
 exec stdbuf -eL python -m booltest.booltest_main $@
