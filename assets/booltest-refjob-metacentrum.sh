3fc7ba6fefda7e9f6b1ae5f438b0a4b3839d3c24
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-20 16:08:35 +0100

    refjob added

---
+++ assets/booltest-refjob-metacentrum.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export HOMEDIR="/storage/brno3-cerit/home/${LOGNAME}"
+export BOOLTEST="${HOMEDIR}/booltest"
+export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams"
+cd $HOMEDIR
+
+export MPICH_NEMESIS_NETMOD=tcp
+export OMP_NUM_THREADS=$PBS_NUM_PPN
+export PYENV_ROOT="${HOMEDIR}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+echo "`hostname` starting..."
+
+module add gcc-5.3.0
+module add cmake-3.6.1
+module add mpc-1.0.3
+module add gmp-6.1.2
+module add mpfr-3.1.4
+
+eval "$(pyenv init -)"
+sleep 3
+
+pyenv local 3.6.4
+sleep 3
+
+
+export HDIR=/storage/brno3-cerit/home/ph4r05/
+export RESDIR=$HDIR/bool-res
+export JOBDIR=$HDIR/bool-jobJ
+mkdir -p $JOBDIR
+
+cd booltest
+exec stdbuf -eL python booltest/testjobs.py --generator-path $HDIR/eacirc-streams/build/eacirc-streams \
+ --data-dir $RESDIR --job-dir $JOBDIR --result-dir=$RESDIR \
+ --top 128 --matrix-size 1 10  --matrix-comb-deg 1 2 3 --matrix-deg 1 2 3 \
+ --no-comb-and --only-top-comb --only-top-deg --no-term-map --topterm-heap \
+ --topterm-heap-k 256 --ref-only --skip-finished
