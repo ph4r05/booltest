f71fe93487f060cf910bb23876a93dd3c1ee4e08
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-22 18:08:00 +0100

    booltest rsync

--- assets/booltest-rsync.sh
+++ assets/booltest-rsync.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+export HOMEDIR="/storage/brno3-cerit/home/${LOGNAME}"
+rsync -av --exclude __pycache__ "${HOMEDIR}/booltest/booltest/" "${HOMEDIR}/.pyenv/versions/3.6.4/lib/python3.6/site-packages/booltest/"
+
