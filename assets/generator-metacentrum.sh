b4ba0a5a4cdf3737dc26a691f60f2e5e6bdb0147
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-16 11:50:41 +0200

    assets: generator metacentrum picks correct binary
    - deb 8, deb 9

--- assets/generator-metacentrum.sh
+++ assets/generator-metacentrum.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export HOMEDIR="/storage/brno3-cerit/home/${LOGNAME}"
-export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams"
+export DEBVER=`cat /etc/debian_version 2>/dev/null | cut -d '.' -f 1`
+export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams-deb${DEBVER}"
 
 module add gcc-5.3.0 2>/dev/null >/dev/null
 module add cmake-3.6.1 2>/dev/null >/dev/null
