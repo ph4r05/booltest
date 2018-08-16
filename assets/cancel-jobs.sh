ddca55f55669b285db7e690bbb376c56dad07fe6
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-16 15:57:57 +0200

    assets: job cancellation batching

--- assets/cancel-jobs.sh
+++ assets/cancel-jobs.sh
@@ -8,10 +8,22 @@ if [[ ( -z "$JOB_START" ) || ( -z "$JOB_END" ) ]]; then
     exit 1
 fi
 
-for i in `seq $JOB_START $JOB_END`; do
-    echo "Canceling job $i"
-    qdel "${i}.arien-pro.ics.muni.cz"
+BATCH=50
+for (( c=JOB_START; c<=JOB_END; c+=BATCH )); do
+    AGG=""
+    AGG_IDS=""
+    for (( i=c; i< (c+$BATCH) && i <= JOB_END; i++ )); do
+        AGG_IDS="$AGG_IDS $i"
+        AGG="$AGG ${i}.arien-pro.ics.muni.cz"
+    done
+    echo "Canceling jobs $AGG_IDS"
+    qdel $AGG
 done
+
+#for i in `seq $JOB_START $JOB_END`; do
+#    echo "Canceling job $i"
+#    qdel "${i}.arien-pro.ics.muni.cz"
+#done
 
 
 
