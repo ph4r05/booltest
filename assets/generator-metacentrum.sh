882f0b885f31fe247ad6f336a8746101f5529bcb
Dusan Klinec <dusan.klinec@gmail.com>
2019-03-27 15:18:56 +0100

    generator - use new binary

--- assets/generator-metacentrum.sh
+++ assets/generator-metacentrum.sh
@@ -2,7 +2,10 @@
 
 export HOMEDIR="/storage/brno3-cerit/home/${LOGNAME}"
 export DEBVER=`cat /etc/debian_version 2>/dev/null | cut -d '.' -f 1`
-export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams-deb${DEBVER}"
+export EACIRC_ESTREAM="${HOMEDIR}/crypto-streams-v3.0"
+#export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams-v2.3"
+#export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams-v3"
+#export EACIRC_ESTREAM="${HOMEDIR}/eacirc-streams/build/eacirc-streams-deb${DEBVER}"
 
 module add gcc-5.3.0 2>/dev/null >/dev/null
 module add cmake-3.6.1 2>/dev/null >/dev/null
