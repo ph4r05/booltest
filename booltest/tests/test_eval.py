8a8b232d9ad605c8e0a986bd320c2049d0355688
Dusan Klinec <dusan.klinec@gmail.com>
2019-03-28 23:33:02 +0100

    tests: minor eval test added

--- booltest/tests/test_eval.py
+++ booltest/tests/test_eval.py
@@ -18,8 +18,29 @@ class TermEvalTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TermEvalTest, self).__init__(*args, **kwargs)
 
-    def test_nothing(self):
-        self.assertTrue(1)
+    def test_eval1(self):
+        data = b'\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0'  # 12 x \xf0
+        data_bin = common.to_bitarray(data)
+
+        te = common.TermEval(16, 2)
+        te.load(data_bin)
+        r1 = te.eval_terms(1)
+        self.assertEqual(r1[0], 6)
+        self.assertEqual(r1[1], 6)
+        self.assertEqual(r1[5], 0)
+
+        r2 = te.eval_terms(2)
+        self.assertEqual(r2[0], 6)  # x0x1
+        self.assertEqual(r2[4], 0)  # x0x5
+
+        r3 = te.eval_terms(3)
+        self.assertEqual(r3[0], 6)
+        self.assertEqual(r3[2], 0)  # x0x1x4
+
+        r2x = te.eval_all_terms(3)
+        self.assertEqual(r1, r2x[1])
+        self.assertEqual(r2, r2x[2])
+        self.assertEqual(r3, r2x[3])
 
 
 if __name__ == "__main__":
