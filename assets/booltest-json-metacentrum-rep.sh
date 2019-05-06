b54fe92ec45e520efdc06aa82ea502af45e743b0
Dusan Klinec <dusan.klinec@gmail.com>
2019-05-06 13:32:34 +0200

    jobs: job retry - track seconds elapsed

--- assets/booltest-json-metacentrum-rep.sh
+++ assets/booltest-json-metacentrum-rep.sh
@@ -12,11 +12,18 @@ export PATH="${PYENV_ROOT}/bin:${PATH}"
 
 C_ITER=0
 RETCODE=2
+TIME_START=$SECONDS
 
 while (( $C_ITER < 6 && ($RETCODE == 2 || $RETCODE == 1) )); do
 
     C_ITER=$((C_ITER+1))
     echo "`hostname` starting ${C_ITER}..."
+
+    TIME_ELAPSED=$(($SECONDS - $TIME_START))
+    if (( $TIME_ELAPSED > 600 )); then
+        echo "Elapsed time too big: ${TIME_ELAPSED}, quitting"
+        break
+    fi
 
     module add gcc-5.3.0
     module add cmake-3.6.1
